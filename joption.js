573db2fdbfa62316b0f4dc66ce4e585ca86550a6
Diego Guevara <diego.guevara@ritbox.com>
2010-11-05 19:40:36 -0500

    first version

---
+++ joption.js
@@ -0,0 +1,84 @@
+/*!
+* @(#) jOption jQuery plugin v1.0.0
+* http://www.ritbox.com
+*
+* Copyright 2010, Diego Guevara - Ritbox Ltda.
+* Released under dual licensed under the MIT or GPL Version 2 licenses.
+*
+* Creation Date: Jul.02.2010
+* Last Update: Jun.06.2010
+*/
+
+ (function($){
+    $.fn.addOptions = function(options){
+
+        var defaults =
+        {
+            url: '',
+            data: '',
+            selected: '',
+            callback: ''
+        };
+        //var options;
+        options = $.extend(defaults, options);
+
+
+
+
+        var obj = this;
+        var value_active = '';
+
+        if (options.url != '')
+        {
+            $.ajax(
+            {
+                type: "GET",
+                url: options.url,
+                dataType: 'json',
+                cache: false,
+                success: function(data){
+                    loadOptions(obj, data, options);
+
+                    if (options.callback != '')
+                    options.callback(true);
+                },
+                error: function(){
+                    if (options.callback != '')
+                    options.callback(false);
+                }
+            });
+
+        }
+        else
+        {
+            loadOptions(obj, options.data, options);
+            if (options.callback != '')
+            options.callback(true);
+        }
+
+    };
+
+    $.fn.addOption = function(value, text, selected){
+        selvalue = (selected) ? "selected": "";
+        value_active = '<option value="' + value + '"' + selvalue + '>';
+        value_active += text;
+        value_active += '</option>';
+
+        this.append(value_active);
+    };
+
+    var loadOptions = function(obj, data, options){
+        //obj.html('');
+
+        $.each(data,
+        function(thevalue, thetext){
+            selvalue = (thevalue == options.selected) ? "selected": "";
+            value_active = '<option value="' + thevalue + '"' + selvalue + '>';
+            value_active += thetext;
+            value_active += '</option>';
+
+            obj.append(value_active);
+
+        });
+    }
+})(jQuery);
